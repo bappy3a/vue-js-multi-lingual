6b45f3c52f78ff1bec8880198d5e99eb6367b756
bappy3a <sunnobappy@gmail.com>
2022-02-01 16:05:51 +0600

    how to multi lingual in vue js

---
+++ resources/js/components/demo.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+
+    <h1>{{ $t('hello') }}</h1>
+</div>
+
+</template>
+
+<script>
+export default {
+    methods:{
+        changeLanguage(obj){
+            localStorage.setItem('language',obj.target.value)
+        }
+    }
+}
+</script>
